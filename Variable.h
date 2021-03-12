ed686007cf515219c9ddb5b818ee541d485c7b2b
Boaz Kaufman <boaz.kaufman97@myhunter.cuny.edu>
2021-03-12 18:31:08 -0500

    Add variable constructor

--- Variable.h
+++ Variable.h
@@ -4,4 +4,11 @@ class Variable {
    public:
     string type;
     int addr_offset;
+
+    Variable(string t, int a);
 };
+
+Variable::Variable(string t, int a){
+  type = t;
+  addr_offset = a;
+}
