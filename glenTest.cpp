65ba239df7e6ffa6365ceae46e99ba4e9dbb3c63
Mariglen Jahaj <Mariglen.Jahaj98@myhunter.cuny.edu>
2021-03-19 17:30:30 -0400

    removed arthithmatic

--- glenTest.cpp
+++ glenTest.cpp
@@ -1,9 +1,19 @@
 #include <iostream>
 #include <sstream>
 #include <vector>
+#include <regex>
+#include <string>
 #include "Variable.h"
 #include "Function.h"
 using namespace std;
+
+int label_num = 2;
+
+vector<string> split(const string str, const string regex_str) {
+   regex regexz(regex_str);
+   vector<string> list(sregex_token_iterator(str.begin(), str.end(), regexz, -1), sregex_token_iterator());
+   return list;
+}
 
 void printVec(vector<string> vec){
     for(int i = 0; i < vec.size(); i++){
@@ -11,217 +21,45 @@ void printVec(vector<string> vec){
     }
 }
                                 //Function &f1
-void arithmetic_handler(string line, Function &f1) {
-
-    string lineToParse = line;
-    vector<string> parsedLine;//[0] = a, [2] = b, [3] = + , [4] = c {a = b + c}
-
-    lineToParse.erase(lineToParse.length() - 1, lineToParse.length());// remove ";"
-
-    string word;
-    istringstream iss(lineToParse);
-    while(iss >> word){// split the string into a vector
-        parsedLine.push_back(word.c_str());
-    }
 
 
-    if(parsedLine[3] == "+"){
-        if(isdigit(parsedLine[2].at(0)) && isdigit(parsedLine[4].at(0))){//const + const
-            int c3 = stoi(parsedLine[2]) + stoi(parsedLine[4]);//add both consts
-            auto itr = f1.variables.find(parsedLine[0]);//get c3
-            string insStr = "movl $" + to_string(c3) + ", " + to_string(itr->second.addr_offset) + "(%rbp)";
-            f1.assembly_instructions.push_back(insStr);
-            itr->second.value = c3;
- cout << "1" << endl;           
-        }else if(!isdigit(parsedLine[2].at(0)) && isdigit(parsedLine[4].at(0))){// var + const
-            auto itr = f1.variables.find(parsedLine[0]);//c3 var
-            auto itr2 = f1.variables.find(parsedLine[2]);//c1 var
-            int c3 = itr2->second.value + stoi(parsedLine[4]);//new val
-            if(parsedLine[0] == parsedLine[2]){
-                string insStr = "addl $" + parsedLine[4] + "," + to_string(itr->second.addr_offset) + "(%rbp)";
-                f1.assembly_instructions.push_back(insStr);
-                cout << insStr << endl;
-cout << "2" << endl;   
-            }else{
-                string insStr = "movl " + to_string(itr2->second.addr_offset) + "(%rbp), %eax";
-                f1.assembly_instructions.push_back(insStr);
-                insStr = "addl $" + parsedLine[4] + ", %eax";
-                f1.assembly_instructions.push_back(insStr);
-                insStr = "movl %eax "+ to_string(itr->second.addr_offset) + "(%rbp)";
-                f1.assembly_instructions.push_back(insStr);
-cout << "3" << endl;   
-            }
-            itr->second.value = c3;//replace val
-            
-        }else if(isdigit(parsedLine[2].at(0)) && !isdigit(parsedLine[4].at(0))){//const + var
-            auto itr = f1.variables.find(parsedLine[0]);//c3 var
-            auto itr2 = f1.variables.find(parsedLine[4]);//c2 var
-            int c3 = stoi(parsedLine[2]) + itr2->second.value;
-cout << "4" << endl;
-            if(parsedLine[0] == parsedLine[4]){
-                string insStr = "addl $" + parsedLine[2] + "," + to_string(itr->second.addr_offset) + "(%rbp)";
-                f1.assembly_instructions.push_back(insStr);
-            }else{
-                string insStr = "movl " + to_string(itr2->second.addr_offset) + "(%rbp), %eax";
-                f1.assembly_instructions.push_back(insStr);
-                insStr = "addl $" + parsedLine[2] + ", %eax";
-                f1.assembly_instructions.push_back(insStr);
-                insStr = "movl %eax "+ to_string(itr->second.addr_offset) + "(%rbp)";
-                f1.assembly_instructions.push_back(insStr);
-            }
-            itr->second.value = c3;
-        }else{//both var
-            if (parsedLine[0] == parsedLine[2] && parsedLine[0] != parsedLine[4]){//x = x + var
-cout << "5" << endl;   
-                auto itr = f1.variables.find(parsedLine[0]);//c3 var
-                auto itr1 = f1.variables.find(parsedLine[2]);//c1 var
-                auto itr2 = f1.variables.find(parsedLine[4]);//c2 var 
-                int c3 = itr1->second.value + itr2->second.value;
-                string insStr = "movl " + to_string(itr2->second.addr_offset) + "(%rbp), %eax";
-                f1.assembly_instructions.push_back(insStr);
-                insStr = "addl %eax " + to_string(itr->second.addr_offset) + "(%rbp)";
-                f1.assembly_instructions.push_back(insStr);
-                itr->second.value = c3;//replace val
-            }else if(parsedLine[0] == parsedLine[4] && parsedLine[0] != parsedLine[2]){//x = var + x
-    cout << "6" << endl;   
-                auto itr = f1.variables.find(parsedLine[0]);//c3 var
-                auto itr1 = f1.variables.find(parsedLine[2]);//c1 var
-                auto itr2 = f1.variables.find(parsedLine[4]);//c2 var
-                int c3 = itr1->second.value + itr2->second.value;
-                string insStr = "movl " + to_string(itr1->second.addr_offset) + "(%rbp), %eax";
-                f1.assembly_instructions.push_back(insStr);
-                insStr = "addl %eax " + to_string(itr2->second.addr_offset) + "(%rbp)";
-                f1.assembly_instructions.push_back(insStr);
-                itr->second.value = c3;//replace val
-            }else if(parsedLine[2] == parsedLine[4]){//both same var 
-     cout << "7" << endl;          
-                auto itr = f1.variables.find(parsedLine[0]);//c3 var
-                auto itr1 = f1.variables.find(parsedLine[2]);//c1 var
-                auto itr2 = f1.variables.find(parsedLine[4]);//c2 var
-                int c3 = itr1->second.value + itr2->second.value;
-                string insStr = "movl " + to_string(itr1->second.addr_offset) + "(%rbp), %eax";
-                f1.assembly_instructions.push_back(insStr);
-                insStr = "addl %eax, %eax";
-                f1.assembly_instructions.push_back(insStr);
-                insStr = "movl %eax " + to_string(itr->second.addr_offset) + "(%rbp)";
-                f1.assembly_instructions.push_back(insStr);
-                itr->second.value = c3;//replace val
-            }else{
-                auto itr = f1.variables.find(parsedLine[0]);//c3 var
-                auto itr1 = f1.variables.find(parsedLine[2]);//c1 var
-                auto itr2 = f1.variables.find(parsedLine[4]);//c2 var 
-                int c3 = itr1->second.value + itr2->second.value;
-                string insStr = "movl " + to_string(itr1->second.addr_offset) + "(%rbp), %edx";
-                f1.assembly_instructions.push_back(insStr);
-                insStr = "movl " + to_string(itr2->second.addr_offset) + "(%rbp), %eax";
-                f1.assembly_instructions.push_back(insStr);
-                insStr = "addl %edx, %eax";
-                f1.assembly_instructions.push_back(insStr);
-                insStr = "movl %eax " + to_string(itr->second.addr_offset) + "(%rbp)";
-                f1.assembly_instructions.push_back(insStr);
-                itr->second.value = c3;//replace val
-      
-            }
+/*
+    Handle for statements
+*/
+void FOR_statement_handler(string source, int max_len, Function &f1, int &addr_offset) {
+    string loop_label = ".L" + to_string(label_number++);
+    string end_label = ".L" + to_string(label_number++);
+    
+    // for (int i =0; i < 5; i++)
+    // int i = 0;
+    // push loop label
+    // i < 5
+    // jump end_label
+    // i = i + 1
 
-        }
+    // loc++;  // go to next source code line
+    // bool next_function = false;
+    // while (loc < max_len) {
+    //     if ((source[loc].find("int") == 0 || source[loc].find("void") == 0) && source[loc].find("}") == source[loc].length() - 1) {
+    //         // start with a new function
+    //         next_function = true;
+    //         break;
+    //     } else if (source[loc] == "}") {
+    //         loc++;
+    //     } else {
+    //         // line is not function call or function end
+    //         // send to common handler dispatcher
+    //         common_instruction_handler_dispatcher(source, loc, max_len, f1, addr_offset);
+    //     }
+    // }
 
-    }else if(parsedLine[3] == "-"){
-         if(isdigit(parsedLine[2].at(0)) && isdigit(parsedLine[4].at(0))){//const - const
-            int c3 = stoi(parsedLine[2]) - stoi(parsedLine[4]);//sub both consts
-            auto itr = f1.variables.find(parsedLine[0]);//get c3
-            string insStr = "movl $" + to_string(c3) + ", " + to_string(itr->second.addr_offset) + "(%rbp)";
-            f1.assembly_instructions.push_back(insStr);
-            itr->second.value = c3;
-        }else if(!isdigit(parsedLine[2].at(0)) && isdigit(parsedLine[4].at(0))){// var - const
-            auto itr = f1.variables.find(parsedLine[0]);//c3 var
-            auto itr2 = f1.variables.find(parsedLine[2]);//c1 var
-            int c3 = itr2->second.value - stoi(parsedLine[4]);//new val
-            
-            if(parsedLine[0] == parsedLine[2]){
-                string insStr = "subl $" + parsedLine[4] + "," + to_string(itr->second.addr_offset) + "(%rbp)";
-                f1.assembly_instructions.push_back(insStr);
-                
-            }else{
-                string insStr = "movl " + to_string(itr2->second.addr_offset) + "(%rbp), %eax";
-                f1.assembly_instructions.push_back(insStr);
-                insStr = "subl $" + parsedLine[4] + ", %eax";
-                f1.assembly_instructions.push_back(insStr);
-                insStr = "movl %eax "+ to_string(itr->second.addr_offset) + "(%rbp)";
-                f1.assembly_instructions.push_back(insStr);
-                
-            }
-            itr->second.value = c3;//replace val
-            
-        }else if(isdigit(parsedLine[2].at(0)) && !isdigit(parsedLine[4].at(0))){//const - var
-            auto itr = f1.variables.find(parsedLine[0]);//c3 var
-            auto itr2 = f1.variables.find(parsedLine[4]);//c2 var
-            int c3 = stoi(parsedLine[2]) - itr2->second.value;
+    // finished the for loop, already hit the }
+    // unconditional jump back to loop_label  jmp     loop_label
+    // push end_label
 
-            if(parsedLine[0] == parsedLine[4]){
-                string insStr = "subl $" + parsedLine[2] + "," + to_string(itr->second.addr_offset) + "(%rbp)";
-                f1.assembly_instructions.push_back(insStr);
-            }else{
-                string insStr = "movl " + to_string(itr2->second.addr_offset) + "(%rbp), %eax";
-                f1.assembly_instructions.push_back(insStr);
-                insStr = "subl $" + parsedLine[2] + ", %eax";
-                f1.assembly_instructions.push_back(insStr);
-                insStr = "movl %eax "+ to_string(itr->second.addr_offset) + "(%rbp)";
-                f1.assembly_instructions.push_back(insStr);
-            }
-            itr->second.value = c3;
-        }else{//both var
-            if (parsedLine[0] == parsedLine[2] && parsedLine[0] != parsedLine[4]){//x = x - var
-                auto itr = f1.variables.find(parsedLine[0]);//c3 var
-                auto itr1 = f1.variables.find(parsedLine[2]);//c1 var
-                auto itr2 = f1.variables.find(parsedLine[4]);//c2 var 
-                int c3 = itr1->second.value - itr2->second.value;
-                string insStr = "movl " + to_string(itr2->second.addr_offset) + "(%rbp), %eax";
-                f1.assembly_instructions.push_back(insStr);
-                insStr = "subl %eax " + to_string(itr->second.addr_offset) + "(%rbp)";
-                f1.assembly_instructions.push_back(insStr);
-                itr->second.value = c3;//replace val
-
-            }else if(parsedLine[0] == parsedLine[4] && parsedLine[0] != parsedLine[2]){//c3=c2
-                auto itr = f1.variables.find(parsedLine[0]);//c3 var
-                auto itr1 = f1.variables.find(parsedLine[2]);//c1 var
-                auto itr2 = f1.variables.find(parsedLine[4]);//c2 var
-                int c3 = itr1->second.value - itr2->second.value;
-
-                string insStr = "movl " + to_string(itr1->second.addr_offset) + "(%rbp), %eax";
-                f1.assembly_instructions.push_back(insStr);
-                insStr = "subl %eax " + to_string(itr2->second.addr_offset) + "(%rbp)";
-                f1.assembly_instructions.push_back(insStr);
-                itr->second.value = c3;//replace val
-            }else if(parsedLine[2] == parsedLine[4]){//both same var 
-                auto itr = f1.variables.find(parsedLine[0]);//c3 var
-                auto itr1 = f1.variables.find(parsedLine[2]);//c1 var
-                auto itr2 = f1.variables.find(parsedLine[4]);//c2 var
-                int c3 = itr1->second.value - itr2->second.value;
-                string insStr = "movl $" + to_string(0) + " " + to_string(itr->second.addr_offset) + "(%rbp)";
-                f1.assembly_instructions.push_back(insStr);
-                itr->second.value = c3;//replace val
-               
-            }else{//both different vars
-                auto itr = f1.variables.find(parsedLine[0]);//c3 var
-                auto itr1 = f1.variables.find(parsedLine[2]);//c1 var
-                auto itr2 = f1.variables.find(parsedLine[4]);//c2 var
-                int c3 = itr1->second.value - itr2->second.value; 
-                string insStr = "movl " + to_string(itr1->second.addr_offset) + "(%rbp), %eax";
-                f1.assembly_instructions.push_back(insStr);
-                insStr = "subl " + to_string(itr2->second.addr_offset) + "(%rbp)" + ", %eax";
-                f1.assembly_instructions.push_back(insStr);
-                insStr = "movl %eax "+ to_string(itr->second.addr_offset) + "(%rbp)";
-                f1.assembly_instructions.push_back(insStr);
-                printVec(f1.assembly_instructions);
-            }
-
-        }   
-
-    }else if(parsedLine[3] == "*"){
-        cout << "mult" << endl;
-    }
 
 }
+
 
 int main(){
     string ln = "c = a * b;";
